6ffb69ab4530308ea9df8e8284e75dc851b457d8
samuel-eric <samueltan086@gmail.com>
2023-11-06 10:41:01 +0700

    Add submission for exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,20 @@ const totalLikes = (blogs) => {
 	return blogs.reduce((sum, item) => sum + item.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+	const likes = blogs.map((blog) => blog.likes)
+	const maxLike = Math.max(...likes)
+	const result = blogs.find((blog) => blog.likes === maxLike)
+	if (result) {
+		delete result._id
+		delete result.url
+		delete result.__v
+	}
+	return result
+}
+
 module.exports = {
 	dummy,
 	totalLikes,
+	favoriteBlog,
 }
